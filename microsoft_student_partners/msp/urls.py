a5083de4e8f7624534d395fa31373809106adaf6
ramanaditya <adityaraman96@gmail.com>
2020-04-13 23:46:26 +0530

    Url,navbar, footer corrected

--- microsoft_student_partners/msp/urls.py
+++ microsoft_student_partners/msp/urls.py
@@ -5,6 +5,14 @@ from microsoft_student_partners.msp.views import MspView
 
 app_name = "msp"
 urlpatterns = [
-    path("", view=MspView.as_view()),
-    path("services/",TemplateView.as_view(template_name="services.html"), name="services")
+    path("", view=MspView.as_view(), name="home"),
+    path(
+        "services/",
+        TemplateView.as_view(template_name="services.html"),
+        name="services",
+    ),
+    path("about", TemplateView.as_view(template_name="about.html"), name="about",),
+    path(
+        "contact", TemplateView.as_view(template_name="contact.html"), name="contact",
+    ),
 ]
