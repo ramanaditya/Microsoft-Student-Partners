1b1f2bb0009cb10ff617087cf36821db19cc0a4f
ramanaditya <adityaraman96@gmail.com>
2020-05-07 00:56:11 +0530

    Azure db Used

--- config/settings/production.py
+++ config/settings/production.py
@@ -22,10 +22,13 @@ ALLOWED_HOSTS = [
 # DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(ROOT_DIR, "db.sqlite3"),
+        "ENGINE": "sql_server.pyodbc",
+        "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("ADMIN_USERNAME"),
         "PASSWORD": os.environ.get("ADMIN_PASSWORD"),
+        "HOST": "myserver.database.windows.net",
+        "PORT": "",
+        "OPTIONS": {"driver": "ODBC Driver 17 for SQL Server",},
     }
 }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
