f2566833fbf028afd842a61c897e8a802ca79413
ramanaditya <adityaraman96@gmail.com>
2020-05-07 03:05:37 +0530

    configured

--- config/settings/production.py
+++ config/settings/production.py
@@ -29,7 +29,7 @@ DATABASES = {
         "HOST": "tcp:mspglobalserver.database.windows.net",
         "PORT": "1433",
         "OPTIONS": {
-            "driver": "ODBC Driver 13 for SQL Server",
+            "driver": "ODBC Driver 17 for SQL Server",
             },
     }
 }
